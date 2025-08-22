21e5221b16a8df39355554d23ac1d4bae18ee3cd
Nhat Minh <pnnhatminh2005@gmail.com>
2025-08-22 20:23:26 +0700

    update mpu.h

--- main/mpu.h
+++ main/mpu.h
@@ -11,30 +11,7 @@
 #define I2C_MASTER_SDA_IO 21
 #define I2C_MASTER_FREQ_HZ 400000
 #define MPU6050_ADDR 0x68
-// esp_err_t i2c_write(uint8_t reg, uint8_t data){
-//     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-//     i2c_master_start(cmd);
-//     i2c_master_write_byte(cmd, (MPU6050_ADDR << 1) | I2C_MASTER_WRITE, true);
-//     i2c_master_write_byte(cmd, reg, true);
-//     i2c_master_write_byte(cmd, data, true);
-//     i2c_master_stop(cmd);
-//     esp_err_t ret = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
-//     i2c_cmd_link_delete(cmd);
-//     return ret;
-// }
-// esp_err_t i2c_read(uint8_t reg, uint8_t *data, size_t len) {
-//     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
-//     i2c_master_start(cmd);
-//     i2c_master_write_byte(cmd, (MPU6050_ADDR << 1) | I2C_MASTER_WRITE, true);
-//     i2c_master_write_byte(cmd, reg, true);
-//     i2c_master_start(cmd); // Repeated start
-//     i2c_master_write_byte(cmd, (MPU6050_ADDR << 1) | I2C_MASTER_READ, true);
-//     i2c_master_read(cmd, data, len, I2C_MASTER_LAST_NACK);
-//     i2c_master_stop(cmd);
-//     esp_err_t ret = i2c_master_cmd_begin(I2C_MASTER_NUM, cmd, 1000 / portTICK_PERIOD_MS);
-//     i2c_cmd_link_delete(cmd);
-//     return ret;
-// }
+
 void kalman_1d(float KalmanState, float KalmanUncertainty, float KalmanInput, float KalmanMeasurement);
 void gyro_signals(void);
 void mpu_read(void);
